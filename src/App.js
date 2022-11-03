fb9c5779ddc6794430addd349890cca3353f1ccd
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-11-03 09:09:06 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,59 +1,72 @@
 import React from 'react';
 import './App.css';
+import {useState} from 'react'
+import Person from './components/Person/person'
 
-const initialState = [
-  {
-    id: 1,
-    person: 'Karolin',
-    order: {
-      main: 'Burrito',
-      protein: 'Organic Tofu',
-      rice: 'Purple Rice',
-      sauce: 'Green Crack',
-      toppings: ['Baby Bok Choy', 'Cucumber Kimchi'],
-      drink: 'Korchata',
-      cost: 22,
-    },
-    paid: false,
-  },
-  {
-    id: 2,
-    person: 'Jerrica',
-    order: {
-      main: 'Rice Bowl',
-      protein: 'Ginger Soy Chix',
-      rice: 'Sticky Rice',
-      sauce: 'Korilla',
-      toppings: ['Yuzu Pickled Sweet Pepper', 'Kale'],
-      drink: 'Korchata',
-      cost: 19,
-    },
-    paid: false,
-  },
-  {
-    id: 3,
-    person: 'Matt',
-    order: {
-      main: 'Salad Bowl',
-      protein: 'Organic Tofu',
-      rice: 'none',
-      sauce: "K'lla",
-      toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
-      drink: 'Sparkling Blood Orange Soda',
-      cost: 20,
-    },
-    paid: false,
-  },
-];
 
 function App() {
+
+  let [persons, setPersons] = useState([
+    {
+      id: 1,
+      person: 'Billie',
+      order: {
+        main: 'Burrito',
+        protein: 'Skirt Steak',
+        rice: 'Purple Rice',
+        sauce: 'Green Crack',
+        toppings: ['Baby Bok Choy', 'Cucumber Kimchi'],
+        drink: 'Thai Iced Tea',
+        cost: 22,
+      },
+      paid: false,
+    },
+    {
+      id: 2,
+      person: 'Kareem',
+      order: {
+        main: 'Rice Bowl',
+        protein: 'Ginger Chicken',
+        rice: 'Sticky Rice',
+        sauce: 'Korilla',
+        toppings: ['Yuzu Pickled Sweet Pepper', 'Kale'],
+        drink: 'Korchata',
+        cost: 19,
+      },
+      paid: false,
+    },
+    {
+      id: 3,
+      person: 'Andrew',
+      order: {
+        main: 'Salad Bowl',
+        protein: 'Organic Tofu',
+        rice: 'none',
+        sauce: "K'lla",
+        toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
+        drink: 'Sparkling Blood Orange Soda',
+        cost: 20,
+      },
+      paid: false,
+    },
+    ]);
+
+
+
   return (
-    <>
-      <header>
-        <h1 className="name">Korilla</h1>
-      </header>
-      <main></main>
-    </>
+    <div className="App">
+
+    {persons.map((s,i) => 
+      <Person 
+      key={i}
+      person={s.person}
+      order={s.order}
+      />
+
+    )}  
+    
+    
+    </div>
   );
 }
 
